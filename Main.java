3e31af283013fa017bc2f792fb7260d9969d7010
Jovi-is-cool <26.jovi.locascio@carolinaday.org>
2024-10-22 15:21:47 +0000

    updates

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main{
     public static void main(String[] args) {
         //palindrome problem #1
-    String str = "tacobilly";
+    String str = "blue";
     String reversed = "";
     for(int i = 0; i< str.length(); i++){
         reversed = reversed + str.substring(str.length() - i - 1, str.length()- i);
@@ -13,12 +13,13 @@ public class Main{
     System.out.println(palin);
 
     //palindrome problem #3
-    boolean isPalin = false;
-    for(int i = 0; i< str.length()/2; i++){
-        if(isPalin = true){}
-            if(str.equals(reversed.substring(i, i + 1)) == true){
-                isPalin = true;
-            }    
+    boolean isPalin = true;
+    for(int i = 0; i < str.length()/2; i++){
+        String first = str.substring(i, i+1);
+        if(first.equals(str.substring(str.length() - i - 1, str.length() - i)) == false){
+                isPalin = false;
+           }
+        
     }
     System.out.println(isPalin);
     }
